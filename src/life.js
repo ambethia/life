70c90a8b6ea84f87a0103e86c05de568837b349b
Jason L Perry <jasper@ambethia.com>
2017-02-22 16:27:53 -0500

    Improve implementation of game

--- src/life.js
+++ src/life.js
@@ -1,16 +1,14 @@
-import { Set, Map, Range } from 'immutable'
+import { Map, Range } from 'immutable'
 
-const N = Range(-1, 2).map((x, i, r) => r.map(y => Map({x, y}))).flatten(true).toSet().delete(Map({x: 0, y: 0}))
-
+const NMAP = Range(-1, 2).flatMap((x, i, r) => r.map(y => Map({x, y}))).toSet()
+const ZERO = Map({x: 0, y: 0})
 const join = (a, b) => Map({ x: a.get('x') + b.get('x'), y: a.get('y') + b.get('y') })
 
 const life = (state) => {
-  const neighbors = state.reduce((cells, cell) => cells.union(N.map(n => join(cell, n))), new Set())
-  const nextState = neighbors.union(state).reduce((cells, cell) => {
-    const count = N.reduce((c, n) => state.has(join(cell, n)) ? c + 1 : c, 0)
-    return state.has(cell) && count === 2 || count === 3 ? cells.add(cell) : cells
-  }, new Set())
-  return nextState
+  return state.flatMap(cell => NMAP.map(n => join(cell, n))).filter(cell => {
+    const count = NMAP.filter(n => !n.equals(ZERO) && state.has(join(cell, n))).size
+    return count === 3 || count === 2 && state.has(cell)
+  })
 }
 
 export default life
