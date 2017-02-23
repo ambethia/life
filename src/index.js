870a2a64d2e7931974d7a7e4d7fab9e3c2bcff7e
Jason L Perry <jasper@ambethia.com>
2017-02-22 22:16:06 -0500

    Cool renderer, but performance is shit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import life from './life'
-import { fromJS } from 'immutable'
+import { List, fromJS } from 'immutable'
 
-const SCALE = 256
-const SPEED = 64
+const SCALE = 128
+const SPEED = 512
+const COUNT = 32
 let scale = 0
 let hue = 0
 
-let state = fromJS([
+let states = List.of(fromJS([
   { x: 1, y: 0 },
   { x: 3, y: 1 },
   { x: 0, y: 2 },
@@ -14,9 +15,10 @@ let state = fromJS([
   { x: 4, y: 2 },
   { x: 5, y: 2 },
   { x: 6, y: 2 }
-]).toSet()
+]).toSet())
 
 const canvas = document.querySelector('canvas')
+const fpsEl = document.querySelector('.fps')
 const context = canvas.getContext('2d')
 
 const resize = () => {
@@ -31,14 +33,36 @@ const worldToView = (x, y) => {
   return [canvas.width / 2 + x * scale, canvas.height / 2 + y * scale]
 }
 
-const draw = () => {
-  // context.clearRect(0, 0, canvas.width, canvas.height)
-  context.fillStyle = `hsl(${hue}, 80%, 40%)`
+let lastCalledTime = new Date().getTime()
+let counter = 0
+let fpsArray = []
+let average = 0
 
-  state.forEach((cell) => {
-    const x = cell.get('x')
-    const y = cell.get('y')
-    context.fillRect(...worldToView(x, y), scale, scale)
+const draw = () => {
+  const delta = (new Date().getTime() - lastCalledTime) / 1000
+  const fps = Math.ceil((1 / delta))
+  lastCalledTime = new Date().getTime()
+
+  if (counter >= 60) {
+    average = Math.ceil(fpsArray.reduce(function (a, b) { return a + b }) / fpsArray.length)
+    fpsEl.textContent = `${average} fps`
+    counter = 0
+  } else {
+    if (fps !== Infinity) {
+      fpsArray.push(fps)
+    }
+    counter++
+  }
+
+  context.clearRect(0, 0, canvas.width, canvas.height)
+
+  states.forEach((state, i) => {
+    context.fillStyle = `hsla(${(hue + Math.round(i / COUNT * 720)) % 360}, 80%, 50%, ${(i / COUNT)})`
+    state.forEach((cell) => {
+      const x = cell.get('x')
+      const y = cell.get('y')
+      context.fillRect(...worldToView(x, y), scale, scale)
+    })
   })
 
   window.requestAnimationFrame(draw)
@@ -46,6 +70,6 @@ const draw = () => {
 window.requestAnimationFrame(draw)
 
 window.setInterval(() => {
-  state = life(state)
-  hue = (hue + 2) % 360
+  hue = (hue + 1) % 360
+  states = states.push(life(states.last())).takeLast(COUNT)
 }, 1000 / SPEED)
